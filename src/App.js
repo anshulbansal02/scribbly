38d2faa1e6794e6f5a5ed790910fc8220f0eab4b
Anshul Bansal <55446476+anshulbansal02@users.noreply.github.com>
2022-11-05 15:57:48 +0530

    Rearranged App component and added connection event toasts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import { RouterProvider } from "react-router-dom";
+import { useEffect, useRef } from "react";
 
 import router from "./routes";
+import { useSocket } from "contexts/SocketContext";
+import { useToaster } from "hooks";
 
-import { SocketProvider } from "contexts/SocketContext";
 import { ToastContainer } from "components";
 
 import "./App.css";
 
 export default function App() {
+    const errorToast = useRef(null);
+
+    const socket = useSocket();
+    const toaster = useToaster();
+
+    useEffect(() => {
+        socket.connect();
+
+        socket.on("connect", () => {
+            if (errorToast.current) {
+                toaster.dismiss(errorToast.current);
+                toaster.toast({
+                    title: "Connected! 🙌",
+                    dismissable: false,
+                });
+                errorToast.current = null;
+            }
+        });
+
+        socket.on("connect_error", () => {
+            if (!errorToast.current) {
+                const toastId = toaster.error({
+                    title: "Connection Error!",
+                    subtitle:
+                        "Cannot connect to the server. Trying to reconnect",
+                    persistant: true,
+                    dismissable: false,
+                });
+                errorToast.current = toastId;
+            }
+        });
+
+        return () => {
+            socket.off("connect");
+            socket.off("connect_error");
+        };
+    }, [socket]);
+
     return (
         <div className="App">
-            <SocketProvider>
-                <RouterProvider router={router} />
-            </SocketProvider>
+            <RouterProvider router={router} />
             <ToastContainer />
         </div>
     );
--- src/contexts/SocketContext.js
+++ src/contexts/SocketContext.js
@@ -6,7 +6,7 @@ const SocketContext = createContext();
 const SOCKET_SERVER_URL = process.env.REACT_APP_SOCKET_SERVER_URL;
 
 const SocketProvider = ({ children }) => {
-    const socket = io(SOCKET_SERVER_URL);
+    const socket = io(SOCKET_SERVER_URL, { autoConnect: false });
 
     return (
         <SocketContext.Provider value={socket}>
