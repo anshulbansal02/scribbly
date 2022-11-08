cc5a6f7410a70c2bb8657597870f0e1c31164516
Anshul Bansal <55446476+anshulbansal02@users.noreply.github.com>
2022-11-08 17:34:22 +0530

    Refractored Board component

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -6,30 +6,40 @@ import useCanvas from "hooks/useCanvas";
 import { getEventCoords } from "utils";
 import { Toolbox } from "components";
 import { useAtomValue } from "jotai";
-import { selectedColorAtom } from "atoms/canvasAtoms";
+import { selectedColorAtom, selectedToolAtom } from "atoms/canvasAtoms";
 
 function Board() {
     const [canvasRef, canvas] = useCanvas();
+    const canvasPaperRef = useRef(null);
+    const pointerRef = useRef(null);
+
     const touchedRef = useRef(false);
 
     const selectedColor = useAtomValue(selectedColorAtom);
+    const selectedTool = useAtomValue(selectedToolAtom);
 
+    // Selected Tool/Color Change
     useEffect(() => {
-        canvas.setColor(selectedColor);
-    }, [selectedColor, canvas]);
+        if (selectedTool === "brush") {
+            canvas.setColor(selectedColor);
+        } else if (selectedTool === "eraser") {
+            canvas.setColor("#fff");
+        } else if (selectedColor === "bucket") {
+        }
+    }, [selectedColor, canvas, selectedTool]);
 
     // Fit canvas to container
-    const canvasPaperRef = useRef(null);
-    const resizeCanvas = () => {
-        const { width, height } =
-            canvasPaperRef.current.getBoundingClientRect();
 
-        const pixelRatio = window.devicePixelRatio;
-
-        canvasRef.current.width = width * pixelRatio;
-        canvasRef.current.height = height * pixelRatio;
-    };
     useEffect(() => {
+        function resizeCanvas() {
+            const { width, height } =
+                canvasPaperRef.current.getBoundingClientRect();
+
+            const pixelRatio = window.devicePixelRatio;
+
+            canvasRef.current.width = width * pixelRatio;
+            canvasRef.current.height = height * pixelRatio;
+        }
         resizeCanvas();
         window.addEventListener("resize", resizeCanvas);
         return () => {
@@ -37,7 +47,7 @@ function Board() {
         };
     }, []);
 
-    // Event Handlers
+    // Canvas Events
     const handleTouch = (e) => {
         touchedRef.current = true;
 
@@ -59,20 +69,29 @@ function Board() {
         }
     };
 
-    const pointerRef = useRef(null);
-
+    // Pointer Events
     function movePointer(e) {
         const tX = e.nativeEvent.offsetX;
         const tY = e.nativeEvent.offsetY;
-        pointerRef.current.style.display = "block";
-
-        pointerRef.current.style.backgroundColor = selectedColor;
-
         pointerRef.current.style.transform = `translate3d(${tX}px, ${tY}px, 0)`;
     }
 
     function hidePointer() {
         pointerRef.current.style.display = "none";
+    }
+
+    function showPointer() {
+        const pointerStyle = pointerRef.current.style;
+        pointerStyle.display = "block";
+        if (selectedTool === "eraser") {
+            pointerStyle.backgroundColor = "#fff";
+            pointerStyle.borderWidth = "1px";
+            pointerStyle.opacity = 0.8;
+        } else {
+            pointerStyle.backgroundColor = selectedColor;
+            pointerStyle.borderWidth = 0;
+            pointerStyle.opacity = 0.5;
+        }
     }
 
     return (
@@ -81,17 +100,16 @@ function Board() {
                 <div
                     className="paper"
                     ref={canvasPaperRef}
+                    onMouseEnter={showPointer}
                     onMouseMove={movePointer}
                     onMouseLeave={hidePointer}
+                    onPointerDown={handleTouch}
+                    onPointerMove={handleDrag}
+                    onPointerUp={handleLift}
+                    onPointerOut={handleLift}
+                    onPointerCancel={handleLift}
                 >
-                    <canvas
-                        ref={canvasRef}
-                        onPointerDown={handleTouch}
-                        onPointerMove={handleDrag}
-                        onPointerUp={handleLift}
-                        onPointerOut={handleLift}
-                        onPointerCancel={handleLift}
-                    />
+                    <canvas ref={canvasRef} />
                     <div className="canvas-pointer" ref={pointerRef}></div>
                 </div>
             </div>
