cbffd9f8a3e11aff6ef28b6857e6340bd7ef93dc
Anshul Bansal <55446476+anshulbansal02@users.noreply.github.com>
2022-11-10 13:03:39 +0530

    Update initial values for canvas atoms

--- src/atoms/canvasAtoms.js
+++ src/atoms/canvasAtoms.js
@@ -1,7 +1,8 @@
+import { ToolsEnum } from "components/Board/config";
 import { atom } from "jotai";
 
-const selectedToolAtom = atom("brush");
-const selectedColorAtom = atom();
-const selectedStrokeAtom = atom();
+const selectedToolAtom = atom(ToolsEnum.BRUSH);
+const selectedColorAtom = atom("#000000");
+const selectedStrokeAtom = atom(10);
 
 export { selectedColorAtom, selectedToolAtom, selectedStrokeAtom };
