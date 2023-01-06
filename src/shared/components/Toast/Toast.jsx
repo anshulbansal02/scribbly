89ac1c161a1016f53f6769c9b56c502eeaa55482
Anshul Bansal <55446476+anshulbansal02@users.noreply.github.com>
2023-01-06 11:20:22 +0530

    Update Toast with css module

--- src/shared/components/Toast/Toast.jsx
+++ src/shared/components/Toast/Toast.jsx
@@ -1,39 +1,39 @@
-import "./toast.css";
+import styles from "./Toast.module.css";
 
 import { ReactComponent as CloseIcon } from "./static/close.svg";
 
 export default function Toast({
     id,
     render,
-    title = "Notification",
-    subtitle,
+    title = "Toast",
+    text,
     icon,
     dismissable = true,
-    backgroundColor = "#fff",
-    color = "#000",
+    styles: customStyles,
     onDismiss,
 }) {
-    const handleDismiss = () => {
-        onDismiss(id);
-    };
-
     return (
-        <div className="toast" style={{ backgroundColor, color }}>
-            <div className="toast-content">
-                {render ?? (
+        <div className={styles.container} style={customStyles}>
+            <div
+                className={styles.content}
+                style={{ paddingLeft: icon ? "12px" : "18px" }}
+            >
+                {render || (
                     <>
-                        <div className="toast-title-row">
-                            {icon}
-                            <h4 className="toast-title">{title}</h4>
+                        {icon && <div className={styles.icon}>{icon}</div>}
+                        <div>
+                            <p className={styles.title}>{title}</p>
+                            {text && <p className={styles.text}>{text}</p>}
                         </div>
-                        {subtitle && (
-                            <p className="toast-subtitle">{subtitle}</p>
-                        )}
                     </>
                 )}
             </div>
+
             {dismissable && (
-                <button className="close-btn" onClick={handleDismiss}>
+                <button
+                    className={styles.dismissBtn}
+                    onClick={() => onDismiss(id)}
+                >
                     <CloseIcon />
                 </button>
             )}
